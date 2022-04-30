569b7fb7cb79b19700162c2f042e0e4b296de725
doomchild <lee.crabtree@gmail.com>
2022-04-30 18:03:53 -0500

    Added documentation and updated tests

--- src/TaskExtras.cs
+++ src/TaskExtras.cs
@@ -5,27 +5,94 @@ namespace RLC.TaskChaining;
 
 public static class TaskExtras
 {
+	/// <summary>
+  /// Produces a faulted <see cref="Task{T}"/> if the <paramref name="predicate"/> returns <code>false</code>.
+  /// </summary>
+  /// <example>This is a handy way to perform validation.  You might do something like the following:
+  /// <code>
+  /// Task.FromResult(someString)
+  ///   .Then(RejectIf(
+  ///			string.IsNullOrWhitespace,
+  ///			s => new Exception($"nameof(someString) must not be blank")
+  ///   ));
+  /// </code>
+  /// </example>
+  /// <typeparam name="T">The task's underlying type.</typeparam>
+  /// <param name="predicate">A predicate to evaluate with the input argument.</param>
+  /// <param name="rejectionMorphism">A function that takes an <see cref="Exception"/> and returns a
+  /// <typeparamref name="T"/>.</param>
+  /// <returns>A function that performs rejection.</returns>
 	public static Func<T, Task<T>> RejectIf<T>(
 		Predicate<T> predicate,
-		Func<T, Exception> rejectionSupplier
+		Func<T, Exception> rejectionMorphism
 	) => value => predicate(value)
-		? Task.FromException<T>(rejectionSupplier(value))
+		? Task.FromException<T>(rejectionMorphism(value))
 		: Task.FromResult(value);
 
+	/// <summary>
+  /// Allows a faulted <see cref="Task{T}"/> to transform its <see cref="Exception"/> into a different type of
+  /// <see cref="Exception"/> if the <paramref name="predicate"/> returns <code>true</code>.
+  /// </summary>
+  /// <remarks>This is intended to be used with <code>Task.Catch</code> to allow for converting one
+  /// <see cref="Exception"/> into another.  For example, a <see cref="System.Web.HttpException"/> could be wrapped
+  /// with a custom type that adds extra context.</remarks>
+  /// <typeparam name="T">The task's underlying type.</typeparam>
+  /// <param name="predicate">A predicate to evaluate with the <see cref="Task{T}"/>'s <see cref="Exception"/>.</param>
+  /// <param name="rerejectionMorphism">A function that takes an <see cref="Exception"/> and returns an
+  /// <see cref="Exception"/>.</param>
+  /// <returns>A function that performs re-rejection.</returns>
 	public static Func<Exception, Task<T>> ReRejectIf<T>(
 		Predicate<Exception> predicate,
-		Func<Exception, Exception> rerejectionSupplier
+		Func<Exception, Exception> rerejectionMorphism
 	) => exception => predicate(exception)
-		? Task.FromException<T>(rerejectionSupplier(exception))
+		? Task.FromException<T>(rerejectionMorphism(exception))
 		: Task.FromException<T>(exception);
 
+	/// <summary>
+	/// Produces a fulfilled <see cref="Task{T}"/> using the output of the <paramref name="fulfillmentMorphism"/> if the
+	/// <paramref name="predicate"/> returns <code>true</code>.
+	/// </summary>
+	/// <example>This is a handy way to return to a non-faulted state.  You might do something like the following:
+	/// <code>
+	/// Task.FromResult(someUrl)
+	///   .Then(httpClient.GetAsync)
+	///   .Catch(ResolveIf(
+	///			exception => exception is HttpRequestException,
+	///			exception => someFallbackValue
+	///   ));
+	/// </code>
+	/// </example>
+	/// <typeparam name="T">The task's underlying type.</typeparam>
+	/// <param name="predicate">A predicate to evaluate with the <see cref="Task{T}"/>'s <see cref="Exception"/>.</param>
+	/// <param name="fulfillmentMorphism">A function that takes an <see cref="Exception"/> and returns a
+  /// <typeparamref name="T"/>.</param>
+	/// <returns>A function that performs fulfillment.</returns>
 	public static Func<Exception, Task<T>> ResolveIf<T>(
 		Predicate<Exception> predicate,
-		Func<Exception, T> resolutionSupplier
+		Func<Exception, T> fulfillmentMorphism
 	) => value => predicate(value)
-		? Task.FromResult(resolutionSupplier(value))
+		? Task.FromResult(fulfillmentMorphism(value))
 		: Task.FromException<T>(value);
 
+	/// <summary>
+	/// Produces a fulfilled <see cref="Task{T}"/> using the output of the <paramref name="fulfillmentMorphism"/> if the
+	/// <paramref name="predicate"/> returns <code>true</code>.
+	/// </summary>
+	/// <example>This is a handy way to return to a non-faulted state.  You might do something like the following:
+	/// <code>
+	/// Task.FromResult(someUrl)
+	///   .Then(httpClient.GetAsync)
+	///   .Catch(ResolveIf(
+	///			exception => exception is HttpRequestException,
+	///			exception => someFallbackValue
+	///   ));
+	/// </code>
+	/// </example>
+	/// <typeparam name="T">The task's underlying type.</typeparam>
+	/// <param name="predicate">A predicate to evaluate with the <see cref="Task{T}"/>'s <see cref="Exception"/>.</param>
+	/// <param name="fulfillmentMorphism">A function that takes an <see cref="Exception"/> and returns a
+	/// <typeparamref name="T"/>.</param>
+	/// <returns>A function that performs fulfillment.</returns>
 	public static Func<Exception, Task<T>> ResolveIf<T>(
 		Predicate<Exception> predicate,
 		Func<Exception, Task<T>> resolutionSupplier
@@ -33,9 +100,23 @@ public static class TaskExtras
 		? resolutionSupplier(value)
 		: Task.FromException<T>(value);
 
+	/// <summary>
+  /// A function that executes the <paramref name="supplier"/> after <paramref name="deferTime"/> has elapsed.
+  /// </summary>
+  /// <typeparam name="T">The task's underlying type.</typeparam>
+  /// <param name="supplier">A supplier function.</param>
+  /// <param name="deferTime">The length of time to defer execution.</param>
+  /// <returns>A function that performs deferred execution.</returns>
 	public static Task<T> Defer<T>(Func<T> supplier, TimeSpan deferTime) =>
 		Defer(() => Task.FromResult(supplier()), deferTime);
 
+	/// <summary>
+	/// A function that executes the <paramref name="supplier"/> after <paramref name="deferTime"/> has elapsed.
+	/// </summary>
+	/// <typeparam name="T">The task's underlying type.</typeparam>
+	/// <param name="supplier">A supplier function.</param>
+	/// <param name="deferTime">The length of time to defer execution.</param>
+	/// <returns>A function that performs deferred execution.</returns>
 	public static Task<T> Defer<T>(Func<Task<T>> supplier, TimeSpan deferTime) =>
 		Task.Run(async () =>
 		{
@@ -46,7 +127,7 @@ public static class TaskExtras
 
   private static Task<T> DoRetry<T>(
 		Func<Task<T>> supplier,
-		RetryOptions retryOptions,
+		RetryParams retryOptions,
 		Exception? exception,
 		int attempts = 0
 	)
@@ -73,15 +154,41 @@ public static class TaskExtras
         ));
   }
 
-	public static Task<T> Retry<T>(Func<Task<T>> supplier, RetryOptions retryOptions) =>
-		DoRetry(supplier, retryOptions, null, 0);
+  /// <summary>
+	/// A function that performs retries of the <paramref name="supplier"/> if it fails.
+	/// </summary>
+	/// <typeparam name="T">The task's underlying type.</typeparam>
+	/// <param name="supplier">A supplier function.</param>
+	/// <param name="retryParams">The retry parameters.</param>
+	/// <returns>A function that retries execution of the <paramref name="supplier"/>.</returns>
+	public static Task<T> Retry<T>(Func<Task<T>> supplier, RetryParams retryParams) =>
+		DoRetry(supplier, retryParams, null, 0);
 
+  /// <summary>
+	/// A function that performs retries of the <paramref name="supplier"/> if it fails using the default retry parameters.
+	/// </summary>
+	/// <typeparam name="T">The task's underlying type.</typeparam>
+	/// <param name="supplier">A supplier function.</param>
+	/// <returns>A function that retries execution of the <paramref name="supplier"/>.</returns>
 	public static Task<T> Retry<T>(Func<Task<T>> supplier) =>
-		Retry(supplier, RetryOptions.Default);
+		Retry(supplier, RetryParams.Default);
 
-	public static Task<T> Retry<T>(Func<T> supplier, RetryOptions retryOptions) =>
-		Retry(() => Task.Run(supplier), retryOptions);
+  /// <summary>
+	/// A function that performs retries of the <paramref name="supplier"/> if it fails.
+	/// </summary>
+	/// <typeparam name="T">The task's underlying type.</typeparam>
+	/// <param name="supplier">A supplier function.</param>
+	/// <param name="retryParams">The retry parameters.</param>
+	/// <returns>A function that retries execution of the <paramref name="supplier"/>.</returns>
+	public static Task<T> Retry<T>(Func<T> supplier, RetryParams retryParams) =>
+		Retry(() => Task.Run(supplier), retryParams);
 
+  /// <summary>
+	/// A function that performs retries of the <paramref name="supplier"/> if it fails using the default retry parameters.
+	/// </summary>
+	/// <typeparam name="T">The task's underlying type.</typeparam>
+	/// <param name="supplier">A supplier function.</param>
+	/// <returns>A function that retries execution of the <paramref name="supplier"/>.</returns>
 	public static Task<T> Retry<T>(Func<T> supplier) =>
-		Retry(supplier, RetryOptions.Default);
+		Retry(supplier, RetryParams.Default);
 }
--- src/TaskStatics.cs
+++ src/TaskStatics.cs
@@ -5,21 +5,62 @@ namespace RLC.TaskChaining;
 
 public static class TaskStatics
 {
+  /// <summary>
+  /// Wraps a value in a supplier function.
+  /// </summary>
+  /// <typeparam name="T">The type of the supplied value.</typeparam>
+  /// <param name="value">The value to supply.</param>
+  /// <returns>A supplier function.</returns>
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static Func<T> Constant<T>(T value) => () => value;
 
+  /// <summary>
+  /// Wraps a value in a <typeparamref name="T"/> -> <typeparamref name="U"/> function that ignores the input value.
+  /// </summary>
+  /// <typeparam name="T">The input type of the resulting function.</typeparam>
+  /// <typeparam name="U">The output type of the resulting function.</typeparam>
+  /// <param name="value">A function that ignores its input argument and returns <paramref name="value"/></param>
+  /// <returns></returns>
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static Func<T, U> Constant<T, U>(U value) => _ => value;
 
+  /// <summary>
+  /// Returns its input value.
+  /// </summary>
+  /// <typeparam name="T">Type of the input value.</typeparam>
+  /// <param name="value">The value to return.</param>
+  /// <returns>The input value.</returns>
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static T Identity<T>(T value) => value;
 
+  /// <summary>
+  /// Executes the input supplier function.
+  /// </summary>
+  /// <typeparam name="T">The output type of the supplier function.</typeparam>
+  /// <param name="supplier">The supplier function to execute.</param>
+  /// <returns></returns>
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static T Invoke<T>(Func<T> supplier) => supplier();
 
+  /// <summary>
+  /// Composes two functions together.
+  /// </summary>
+  /// <typeparam name="TA">The input type of the first function.</typeparam>
+  /// <typeparam name="TB">The output type of the first function and the input type of the second function.</typeparam>
+  /// <typeparam name="TC">The output type of the second function.</typeparam>
+  /// <param name="f">The first function to compose.</param>
+  /// <param name="g">The second function to compose.</param>
+  /// <returns>The composition of both functions.</returns>
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static Func<TA, TC> Pipe2<TA, TB, TC>(Func<TA, TB> f, Func<TB, TC> g) => x => g(f(x));
 
+  /// <summary>
+  /// Wraps an <see cref="Action{TTappedValue}"/> in a <see cref="Func{TTappedValue, TTappedValue}"/> that executes the
+  /// Action and then returns the input value.
+  /// </summary>
+  /// <typeparam name="TTappedValue">The type of the value passed into the Action and returned.</typeparam>
+  /// <param name="consumer">The Action to perform on the input value.</param>
+  /// <returns>A function that takes a value, executes the <param name="consumer"/>, and returns the value.</returns>
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public static Func<TTappedValue, TTappedValue> Tap<TTappedValue>(Action<TTappedValue> consumer)
   {
