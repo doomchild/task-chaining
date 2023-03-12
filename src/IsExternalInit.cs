6f17d9c6c0d88831a01cc3cbcbbbf75bb13532ad
doomchild <lee.crabtree@gmail.com>
2023-03-11 20:16:39 -0600

    Added missing documentation to IsExternalInit

--- src/IsExternalInit.cs
+++ src/IsExternalInit.cs
@@ -2,5 +2,6 @@
 
 namespace System.Runtime.CompilerServices;
 
+// NOTE:  This is here to enable using C# 9 and above.  See https://www.mking.net/blog/error-cs0518-isexternalinit-not-defined for a more complete explanation.
 [EditorBrowsable(EditorBrowsableState.Never)]
 internal static class IsExternalInit { }
