f1d7d9522f6dd05b52e266f46556eb90bdffe0d7
doomchild <lee.crabtree@gmail.com>
2024-04-16 14:28:39 -0500

    Updated Tap/IfFulfilled/IfFaulted to correctly handle canceled tasks

---
+++ src/TaskExtensionsIfFulfilled.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RLC.TaskChaining;
+
+using static TaskStatics;
+
+public static partial class TaskExtensions
+{
+  /// <summary>
+  /// Performs an action if the <see name="Task{T}"/> is in a fulfilled state.
+  /// </summary>
+  /// <typeparam name="T">The task's underlying type.</typeparam>
+  /// <param name="consumer">The action to perform if the task is fulfilled.</param>
+  /// <returns>The task.</returns>
+  public static Task<T> IfFulfilled<T>(this Task<T> task, Action<T> consumer)
+    => task.ResultMap(TaskStatics.Tap(consumer));
+
+  /// <summary>
+  /// Executes a function and throws away the result if the <see name="Task{T}"/> is in a fulfilled state.
+  /// </summary>
+  /// <typeparam name="T">The task's underlying type.</typeparam>
+  /// <typeparam name="R">The type of the discarded result of <paramref name="func"/>.</typeparam>
+  /// <param name="task">The task.</param>
+  /// <param name="func">The function to execute if the task is fulfilled.</param>
+  /// <returns>The task.</returns>
+  public static Task<T> IfFulfilled<T, R>(this Task<T> task, Func<T, Task<R>> func)
+    => task.ContinueWith(async continuationTask =>
+    {
+      if (continuationTask.IsFaulted || continuationTask.IsCanceled)
+      {
+        return continuationTask;
+      }
+      else
+      {
+        T value = await continuationTask;
+
+        return Task.FromResult(value).Then(func).Then(_ => value, _ => value);
+      }
+    }).Unwrap().Unwrap();
+
+  /// <summary>
+  /// Executes a function and throws away the result if the <see name="Task{T}"/> is in a fulfilled state.
+  /// </summary>
+  /// <typeparam name="T">The task's underlying type.</typeparam>
+  /// <typeparam name="R">The type of the discarded result of <paramref name="func"/>.</typeparam>
+  /// <param name="task">The task.</param>
+  /// <param name="func">The function to execute if the task is fulfilled.</param>
+  /// <returns>The task.</returns>
+  public static Task<T> IfFulfilled<T>(this Task<T> task, Func<T, Task> func)
+    => task.IfFulfilled<T, T>(value => Task.FromResult(value).Then(func).Then(_ => value, _ => value));
+}
---
+++ tests/unit/TaskChainingIfFulfilledTests.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Threading.Tasks;
+
+using RLC.TaskChaining;
+using Xunit;
+
+using static RLC.TaskChaining.TaskStatics;
+
+public class TaskChainingIfFulfilledTests
+{
+  [Fact]
+  public async void ItShouldPerformASideEffect()
+  {
+    int actualValue = 0;
+    int expectedValue = 5;
+
+    await Task.FromResult(5)
+      .IfFulfilled(value =>
+      {
+        actualValue = value;
+      });
+
+    Assert.Equal(expectedValue, actualValue);
+  }
+
+  [Fact]
+  public async void ItShouldPerformASideEffectWithoutAwaiting()
+  {
+    int actualValue = 0;
+    int expectedValue = 5;
+
+    _ = Task.FromResult(5)
+      .IfFulfilled(value =>
+      {
+        actualValue = value;
+      });
+
+    await Task.Delay(2);
+
+    Assert.Equal(expectedValue, actualValue);
+  }
+
+  [Fact]
+  public async void ItShouldNotPerformASideEffectForAFault()
+  {
+    int actualValue = 0;
+    int expectedValue = 0;
+
+    try
+    {
+      await Task.FromException<int>(new ArgumentNullException())
+        .IfFulfilled((int value) =>
+        {
+          actualValue = 5;
+        });
+    }
+    catch (ArgumentNullException)
+    {
+    }
+
+    Assert.Equal(expectedValue, actualValue);
+  }
+
+  [Fact]
+  public async void ItShouldNotPerformASideEffectForAFaultWithoutAwaiting()
+  {
+    int actualValue = 0;
+    int expectedValue = 0;
+
+    _ = Task.FromException<int>(new ArgumentNullException())
+      .IfFulfilled((int _) =>
+      {
+        actualValue = 5;
+      });
+
+    await Task.Delay(2);
+
+    Assert.Equal(expectedValue, actualValue);
+  }
+
+  [Fact]
+  public async void ItShouldNotPerformASideEffectForACancellation()
+  {
+    int actualValue = 0;
+    int expectedValue = 0;
+    Func<int, string> func = _ => throw new TaskCanceledException();
+
+    try
+    {
+      await Task.FromResult<int>(0)
+        .Then(func)
+        .IfFulfilled(_ =>
+        {
+          actualValue = 5;
+        });
+    }
+    catch (OperationCanceledException)
+    {
+    }
+
+    Assert.Equal(expectedValue, actualValue);
+  }
+
+  [Fact]
+  public async void ItShouldNotPerformASideEffectForACancellationWithoutAwaiting()
+  {
+    int actualValue = 0;
+    int expectedValue = 0;
+    Func<int, string> func = _ => throw new TaskCanceledException();
+
+    try
+    {
+      _ = Task.FromResult<int>(0)
+        .Then(func)
+        .IfFulfilled(_ =>
+        {
+          actualValue = 5;
+        });
+    }
+    catch (OperationCanceledException)
+    {
+    }
+
+    await Task.Delay(2);
+
+    Assert.Equal(expectedValue, actualValue);
+  }
+
+  public class WithTaskFunc
+  {
+    [Fact]
+    public async void ItShouldPerformASideEffect()
+    {
+      int actualValue = 0;
+      int expectedValue = 5;
+      Func<int, Task<string>> func = i =>
+      {
+        actualValue = i;
+
+        return Task.FromResult(i.ToString());
+      };
+
+      await Task.FromResult(5)
+        .IfFulfilled(func);
+
+      Assert.Equal(expectedValue, actualValue);
+    }
+
+    [Fact]
+    public async void ItShouldNotPerformASideEffectForAFault()
+    {
+      int actualValue = 0;
+      int expectedValue = 0;
+      Func<int, Task<string>> func = i =>
+      {
+        actualValue = i;
+
+        return Task.FromResult(i.ToString());
+      };
+
+      try
+      {
+        await Task.FromException<int>(new ArgumentNullException())
+          .IfFulfilled(func);
+      }
+      catch (ArgumentNullException)
+      {
+      }
+
+      Assert.Equal(expectedValue, actualValue);
+      }
+
+    [Fact]
+    public async void ItShouldNotResultInAFaultedTaskWithAnException()
+    {
+      Func<int, Task<string>> func = i =>
+      {
+        throw new ArgumentException();
+      };
+
+      int expectedValue = 5;
+      int actualValue = await Task.FromResult(5)
+        .IfFulfilled(func);
+
+      Assert.Equal(expectedValue, actualValue);
+    }
+  }
+
+  public class WithAsyncAction
+  {
+    [Fact]
+    public async void ItShouldContinueAsyncTasks()
+    {
+      string testValue = "12345";
+      int expectedValue = 5;
+      int actualValue = 0;
+      Func<string, Task> func = async value =>
+      {
+        await Task.Delay(1);
+
+        actualValue = value.Length;
+      };
+
+      _ = Task.FromResult(testValue)
+        .IfFulfilled(func);
+
+      await Task.Delay(5);
+
+      Assert.Equal(expectedValue, actualValue);
+    }
+  }
+}
